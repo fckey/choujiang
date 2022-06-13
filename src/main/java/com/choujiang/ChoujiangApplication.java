f2912587a636775bae442a1eb69e19ed374618ee
william Fang <sl.fang@qq.com>
2022-06-13 19:24:41 +0800

    1-1 init

---
+++ src/main/java/com/choujiang/ChoujiangApplication.java
@@ -0,0 +1,13 @@
+package com.choujiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoujiangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoujiangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choujiang/ChoujiangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choujiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoujiangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
